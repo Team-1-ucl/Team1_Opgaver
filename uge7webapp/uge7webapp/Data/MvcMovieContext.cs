47530aa46164d5a2077020d648eb56a97c1748c7
Andreas <Abarbesgaard@gmail.com>
2024-02-13 12:51:47 +0100

    Uge 7

---
+++ uge7webapp/uge7webapp/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using uge7webapp.Models;
+
+namespace uge7webapp.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<uge7webapp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ uge7webapp/uge7webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uge7webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
